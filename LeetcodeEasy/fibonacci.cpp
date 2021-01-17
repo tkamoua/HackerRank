8fabeefac68d0d776006ddd0d5ee7ad8925e5f08
tkamoua <tkamoua@umich.edu>
2021-01-17 15:19:43 -0500

    fibonacci

---
+++ LeetcodeEasy/fibonacci.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        int prev2 = 0;
+        int prev1 = 1; 
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1){
+            return 1;
+        }
+        int curr = 1;
+        for(int i = 2; i <= n; ++i){
+            curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+     return curr;   
+    }
+};
